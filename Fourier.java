696959b17fc3312c51b1c4b7d2eda5111e280e19
Bfzanchetta <bfzanchetta@hotmail.com>
2016-07-14 11:02:55 -0300

    Create Fourier.java

---
+++ Fourier.java
@@ -0,0 +1,17 @@
+package fourier;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author breno
+ */
+public class Fourier {
+
+    ArrayList<Onda> lista = new ArrayList<>();
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
