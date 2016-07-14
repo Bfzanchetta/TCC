3664d86c2e1076364a384ac71740258bb4008ff9
Bfzanchetta <bfzanchetta@hotmail.com>
2016-07-14 11:05:53 -0300

    Update Onda.java

--- Onda.java
+++ Onda.java
@@ -9,17 +9,13 @@ public class Onda {
     private double Amplitude;
     private int frequenciaHertz;
 
-    static int t1, t2;
-
     //Toda faixa de Representacao da onda vai ser estimada e armazenada nessa variavel
     public Onda() {
     }
 
-    public Onda(double Amplitude, int frequenciaHertz, int t1, int t2) {
+    public Onda(double Amplitude, int frequenciaHertz) {
         this.Amplitude = Amplitude;
         this.frequenciaHertz = frequenciaHertz;
-        this.t1 = t1;
-        this.t2 = t2;
     }
 
     public double getAmplitude() {
@@ -36,6 +32,10 @@ public class Onda {
 
     public void setFrequenciaHertz(int frequenciaHertz) {
         this.frequenciaHertz = frequenciaHertz;
+    }
+
+    public void calculaOndaFinal() {
+
     }
 
 }
