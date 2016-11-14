96a829d2de7b310bb72964de1131b33d0ba7ca18
Bfzanchetta <bfzanchetta@hotmail.com>
2016-11-14 12:43:41 -0200

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,11 +1,6 @@
 package trabalhodoleonardo;
 
-/**
- *
- * @author Breno
- */
 public class Node {
-
     static Node root;
     
     public static Node getRoot(){
@@ -14,104 +9,61 @@ public class Node {
         }
         return root;
     }
-    
     String texto;
     Node esquerda;
     Node direita;
 
-    public Node() {
-    }
+    public Node() { }
 
-    public Node(String texto) {
+    public Node(String texto) { 
         this.texto = texto;
         this.esquerda = null;
-        this.direita = null;
-    }
+        this.direita = null; }
 
     public Node(String texto, Node esquerda, Node direita) {
         this.texto = texto;
         this.esquerda = esquerda;
-        this.direita = direita;
-    }
-
-    public String getTexto() {
-        return texto;
-    }
-
-    public void setTexto(String texto) {
-        this.texto = texto;
-    }
-
-    public Node getEsquerda() {
-        return esquerda;
-    }
-
-    public void setEsquerda(Node esquerda) {
-        this.esquerda = esquerda;
-    }
-
-    public Node getDireita() {
-        return direita;
-    }
-
-    public void setDireita(Node direita) {
-        this.direita = direita;
-    }
+        this.direita = direita; }
 
     public boolean verificaFolha() {
         if (this.direita == null && this.esquerda == null) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+            return true; } 
+        else {
+            return false; } }
 
     public Node retornaNodoEsquerda(Node n) {
-        return n.esquerda;
-    }
+        return n.esquerda; }
 
     public Node retornaNodoDireita(Node n) {
-        return n.direita;
-    }
+        return n.direita; }
     
     public boolean temDoisFilhos(Node n){
         if(n.esquerda.verificaTexto(n)==true && n.direita.verificaTexto(n)==true){
-            return true;
-        }
+            return true; }
         else{
-            return false;
-        }
-    }
+            return false; } }
     
     public boolean verificaPergunta(Node n){
         String aux = n.getTexto();
         if(aux.charAt(aux.length()-1)=='?'){
-            return true;
-        }
+            return true; }
         else{
-            return false;
-        }
-    }
+            return false; } }
 
     public boolean verificaTexto(Node n){
         String aux = n.getTexto();
         if(aux.equalsIgnoreCase("")){
-            return false;
-        }
+            return false; }
         else if(aux.equals(null)){
-            return false;
-        }
+            return false; }
+        else if(aux.equals("?")){
+            return false; }
         else{
-            return true;
-        }
-    }
+            return true; } }
     
     @Override
     public boolean equals(Object other) {
         if ((other instanceof Node) == false) {
-            return false;
-        }
+            return false; }
         Node nother = (Node) other;
-        return this.texto.equals(nother.getTexto());
-    }
-}
+        return this.texto.equals(nother.getTexto()); } }
